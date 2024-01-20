4f5b85a8592eb3cb255636c5aab64c68ec4d1066
Oleh Protsak <protsak19@gmail.com>
2024-01-20 11:16:35 +0000

    Finalized the logic for the gatherTeamInfo function.

--- index.js
+++ index.js
@@ -23,18 +23,52 @@ async function gatherTeamInfo() {
   const teamMembers = [];
 
   // Prompt for manager's information
-  const managerAnswers = await inquirer.prompt(managerQuestions);
+  const { name, id, email, officeNumber } = await inquirer.prompt(
+    managerQuestions
+  );
 
   // Create a Manager object and add it to the team
-  const manager = new Manager(
-    managerAnswers.name,
-    managerAnswers.id,
-    managerAnswers.email,
-    managerAnswers.officeNumber
-  );
+  const manager = new Manager(name, id, email, officeNumber);
   teamMembers.push(manager);
   console.log(`${manager.name}, welcome! Let's add members to your team.`);
+
+  let userChoice;
+
+  (async function addTeamMember() {
+    const { choice } = await inquirer.prompt(menuChoiceOptions);
+
+    userChoice = choice;
+
+    if (userChoice === "Add an engineer") {
+      const { name, id, email, github } = await inquirer.prompt(
+        engineerQuestions
+      );
+
+      const engineer = new Engineer(name, id, email, github);
+      teamMembers.push(engineer);
+      console.log(`${engineer.getRole()} successfully added to your team!`);
+      addTeamMember();
+    } else if (userChoice === "Add an intern") {
+      const { name, id, email, school } = await inquirer.prompt(
+        internQuestions
+      );
+
+      const intern = new Intern(name, id, email, school);
+      teamMembers.push(intern);
+      console.log(`${intern.getRole()} successfully added to your team!`);
+      addTeamMember();
+    } else if (userChoice === "Finish building the team") {
+      console.log("Team building complete!");
+      const htmlContent = render(teamMembers);
+
+      if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+        console.log(`Output directory created at: ${OUTPUT_DIR}`);
+      }
+
+      fs.writeFileSync(outputPath, htmlContent);
+    }
+  })();
 }
 
-// Call the function to start gathering team information
 gatherTeamInfo();
